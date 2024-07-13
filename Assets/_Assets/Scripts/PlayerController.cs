89095adefc23d612354bd6e20f8c254237b773cc
hamzarafique57144 <hamzarafique277@gmail.com>
2024-07-13 23:34:31 +0500

    Multiple directions

--- Assets/_Assets/Scripts/PlayerController.cs
+++ Assets/_Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Cinemachine.Utility;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -29,17 +30,49 @@ public class PlayerController : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVector();
         Vector3 movDir = new Vector3(-inputVector.x, 0, -inputVector.y);
         //     Debug.Log("Mov Dir " + movDir);
-        float playerSize = 0.7f;
-        bool canWalk = !Physics.Raycast(transform.position, movDir, playerSize);
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float PlayerRadius = 0.7f;
+        float playerHieght = 2f;
+        bool canWalk = !Physics.CapsuleCast(transform.position,transform.position +Vector3.up* playerHieght, PlayerRadius,movDir, moveDistance);
+        if(!canWalk)
+        {
+            //if player can not move towards movDir then try to move only on x movement
+            Vector3 movDirX = new Vector3(movDir.x, 0, 0).normalized;
+            canWalk = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHieght, PlayerRadius, movDirX, moveDistance);
+            if(canWalk)
+            {
+                movDir = movDirX;
+            }
+            else
+            {
+                
+                //Can not move only on x direction
+                //Atemt only in z
+                Vector3 movDirZ = new Vector3(0,0,movDir.z).normalized;
+                canWalk = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHieght, PlayerRadius, movDirZ, moveDistance);
+                if (canWalk)
+                {
+                    //Can move in z direction
+                    movDir = movDirZ;
+                }
+                else
+                {
+                    //Can not move in any direction
+                    Debug.Log("Player can not move in any direction");
+                }
+
+            }
+
+        }
         transform.forward = Vector3.Slerp(transform.forward, movDir, rotateSpeed * Time.deltaTime);//For player rotation
 
-
-        // transform.forward += movDir * moveSpeed * Time.deltaTime;
-        if (movDir != Vector3.zero)
+         if(canWalk)
         {
-            if(canWalk)
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            //  transform.Translate(Vector3.forward * moveDistance);
+            transform.position += movDir * moveSpeed * Time.deltaTime;
         }
+         
+        
     
         
         /*if(canWalk)
